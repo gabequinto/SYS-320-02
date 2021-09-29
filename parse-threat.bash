322a0b4d1de5d4e228e7aeff082c3ccd312919c3
gabequinto <78443183+gabequinto@users.noreply.github.com>
2021-09-29 18:52:39 -0400

    Create parse-threat.bash

---
+++ parse-threat.bash
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Storyline: Extract IPs from emergingthreats.net and create a firewall ruleset
+#filename variable
+pFile="emerging-drop.suricata.rules"
+# Check if the emerging threats file exists
+if [[ -f "${pFile}" ]]
+then
+        echo " The file ${pFile} exists."
+        echo -n "Would you like to download it? [y|N]"
+        read to_overwrite
+
+        if [[ "${to_overwrite}" == "N" || "${to_overwrite}" == "" ]]
+        then
+
+                exit 0
+
+        elif [[ "${to_overwrite}" == "y" ]]
+        then
+                echo "Downloading......"
+                wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -o /tmp/emerging-drop.suricata.rules
+
+                exit 0
+
+fi
+fi
+
+
+# Regex to extract the networks
+
+#wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -o /tmp/emerging-drop.suricata.rules
+egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0/[0-9]{1,2}' emerging-drop.suricata.rules | sort -u  | tee badIPs.txt
+
+cut -d ',' -f 5 targetedthreats.csv | sort -u | tee badURLs.txt
+
+
+# Create a firewall ruleset
+
+
+while getopts 'icuwm' OPTION ; do
+
+        case "$OPTION" in
+
+
+                i) ip_tables=${OPTION}
+                ;;
+                c) cisco=${OPTION}
+                ;;
+                u) url_filter=${OPTION}
+                ;;
+                w) windows_firewall=${OPTION}
+                ;;
+                m) mac_os=${OPTION}
+                ;;
+                *)
+
+                        echo "Invalid value."
+                        exit 1
+                ;;
+        esac
+done
+
+# Creating list for IP tables
+if [[ ${ip_tables} ]]
+then
+
+
+for eachIP in $(cat badIPs.txt)
+do
+   echo "iptables -A INPUT -s ${eachIP} -j DROP"  | tee -a badIPS.iptables
+
+done
+fi
+
+# Creating list for cisco drop rule
+if [[ ${cisco} ]]
+then
+
+        for eachIP in $(cat badIPs.txt)
+        do
+
+                echo "access-list deny ip any host ${eachIP}" | tee -a badIPs.cisco
+done
+fi
+# Creating list for windows drop rule
+if [[ ${windows_firewall} ]]
+then
+
+        for eachIP in $(cat badIPs.txt)
+        do
+                echo "netsh advfirewall add rule name=BLOCK IP ADDRESS-${eachIP} dir=in action=block ${eachIP}" | tee -a badIPs.windows
+
+done
+fi
+
+
+# Creating list for MAC drop rule
+if [[ ${mac_os} ]]
+then
+
+        for eachIP in $(cat badIPs.txt)
+        do
+                echo "block in from ${eachIP} to any" | tee -a pf.conf
+
+done
+fi
+
+
+# Creating list for cisco url block list
+
+if [[ ${url_filter} ]]
+ then
+        echo "class-map match-any BAD_URLS"
+        for eachURL in $(cat badURLs.txt)
+        do
+                echo "match protocol http host ${eachURL}" | tee -a badurls.urls
+
+done
+fi
+
